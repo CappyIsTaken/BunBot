0132a057071c7bb6f27e6b10e9a24cf088b4fe8b
UwU <alengamer76@gmail.com>
2023-10-06 10:29:20 +0300

    new funnyRussianProgrammer task working...

--- helpers/urls.ts
+++ helpers/urls.ts
@@ -122,7 +122,7 @@ export class InstagramParser extends URLParser {
 
     async getMediaURL() {
         const postID = await this.getPostID()
-        return "https://berd-1337.riouwu.repl.co/insta?id="+postID
+        return "https://berd-1337.riouwu.repl.co/insta/p/?id="+postID
     }
 }
 
--- tasks/funnyRussianProgrammer.ts
+++ tasks/funnyRussianProgrammer.ts
@@ -1,37 +1,33 @@
-// import { AttachmentBuilder } from "discord.js";
-// import { InstagramClient } from "../helpers/instagram";
-// import { Task } from "../structures/task";
-// import {readFile, writeFile} from "fs/promises"
-// const funnyRussianProgrammerTask : Task = {
-//     cron: "0 7 * * *",
-//     async execute(client) {
-//         const instagramClient = InstagramClient.fromUsernameAndPassword(process.env.INSTA_USERNAME, process.env.INSTA_PASSWORD)
-//         const data = await readFile("russianProgrammer.json", {encoding: "utf-8"})
-//         const json = JSON.parse(data)
-//         const userData = await instagramClient.getUser("python_is_trash")
-//         const posts = userData.graphql.user.edge_owner_to_timeline_media.edges
-//         for(let i = 0; i < 5; i++) {
-//             let p = posts[i].node
-//             if(!json.includes(p.shortcode)) {
-//                 if(json.length > 5) {
-//                     json.pop()
-//                 }
-//                 json.push(p.shortcode)
-//                 const postResp = await instagramClient.getPost(p.shortcode)
-//                 const videoUrl = postResp.items[0].video_versions[0].url
-//                 const videoData = await fetch(videoUrl)
-//                 await client.sendMessageToUsers(["279956479140954113", "207188862273978370"], {
-//                     content: "New post from funny russian programmer!",
-//                     files: [new AttachmentBuilder(Buffer.from(await videoData.arrayBuffer()))]
-//                 })
-//             }
-//         }
-//         await writeFile("russianProgrammer.json", JSON.stringify(json), {
-//             encoding: "utf-8"
-//         })
-//     },
-//     timezone: "Asia/Jerusalem",
-//     name: "funnyRussianProgrammer"
-// }
+import { AttachmentBuilder } from "discord.js";
+import { Task } from "../structures/task";
+import {readFile, writeFile} from "fs/promises"
+const funnyRussianProgrammerTask : Task = {
+    cron: "0 7 * * *",
+    async execute(client) {
+        const file = Bun.file("russianProgrammer.json")
+        const json = await file.json()
+        const postIdsResp = await fetch("https://berd-1337.riouwu.repl.co/insta/u?user=python_is_trash&c=5")
+        const postIds = await postIdsResp.json()
+        for(let i = 0; i < postIds.length; i++) {
+            if(!json.includes(postIds[i])) {
+                if(json.length > 5) {
+                    json.pop()
+                }
+                json.push(postIds[i])
 
-// export default funnyRussianProgrammerTask
+                const videoUrl = `https://berd-1337.riouwu.repl.co/insta/p?id=${postIds[i]}`
+                
+                await client.sendMessageToUsers(["279956479140954113", "207188862273978370"], {
+                    content: "New post from funny russian programmer!\n"+videoUrl,
+                })
+            }
+        }
+        await writeFile("russianProgrammer.json", JSON.stringify(json), {
+            encoding: "utf-8"
+        })
+    },
+    timezone: "Asia/Jerusalem",
+    name: "funnyRussianProgrammer"
+}
+
+export default funnyRussianProgrammerTask
